ed421a894dc4a4af159456e1666cb797cbf1cf7e
aluckyguy <12462105+aluckyguy@user.noreply.gitee.com>
2023-12-24 16:28:36 +0800

    添加了jwt认证

--- server/router.js
+++ server/router.js
@@ -1,15 +1,12 @@
 const UserController = require("./controllers/UserController")
 
+const AuthenticatePolicy = require('./policies/AuthenticatePolicy')
+
 module.exports = (app) => {
-    app.get('/api', (req, res) => {
-        res.send({
-            msg: 'Hello, node!'
-        })
-    })
 
     app.post('/users/login', UserController.login)
 
-    app.get('/users/:id', UserController.getUserById)
+    app.get('/users/:id', AuthenticatePolicy.isValidToken, UserController.getUserById)
     app.post('/users', UserController.register)
     app.put('/users/:id', UserController.update)
     app.delete('/users/:id', UserController.delete)
